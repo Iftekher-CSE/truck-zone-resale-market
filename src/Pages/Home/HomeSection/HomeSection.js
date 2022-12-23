3bbdbaacd23427af5ea0cdacdd824899f76b77a1
Iftekher-CSE <108381759+Iftekher-CSE@users.noreply.github.com>
2022-12-23 12:31:01 +0600

    popular brand link

--- src/Pages/Home/HomeSection/HomeSection.js
+++ src/Pages/Home/HomeSection/HomeSection.js
@@ -6,6 +6,7 @@ import brand3 from "../../../Assets/3.webp";
 import brand4 from "../../../Assets/4.webp";
 import brand5 from "../../../Assets/5.webp";
 import brand6 from "../../../Assets/6.webp";
+import { Link } from "react-router-dom";
 
 const HomeSection = () => {
     return (
@@ -13,22 +14,34 @@ const HomeSection = () => {
             <SectionHeader>Popular Brand working with Us</SectionHeader>
             <div className="carousel carousel-center p-4 space-x-4 bg-yellow-300 rounded-box mt-10">
                 <div className="carousel-item">
-                    <img src={brand1} className="rounded-box" />
+                    <a target="_blank" href="https://www.tatamotors.com/">
+                        <img src={brand1} className="rounded-box" />
+                    </a>
                 </div>
                 <div className="carousel-item">
-                    <img src={brand2} className="rounded-box" />
+                    <a target="_blank" href="https://www.ashokleyland.com/">
+                        <img src={brand2} className="rounded-box" />
+                    </a>
                 </div>
                 <div className="carousel-item">
-                    <img src={brand3} className="rounded-box" />
+                    <a target="_blank" href="http://www.eicher.in/">
+                        <img src={brand3} className="rounded-box" />
+                    </a>
                 </div>
                 <div className="carousel-item">
-                    <img src={brand4} className="rounded-box" />
+                    <a target="_blank" href="https://www.smlisuzu.com/">
+                        <img src={brand4} className="rounded-box" />
+                    </a>
                 </div>
                 <div className="carousel-item">
-                    <img src={brand5} className="rounded-box" />
+                    <a target="_blank" href="https://www.mahindratruckandbus.com/">
+                        <img src={brand5} className="rounded-box" />
+                    </a>
                 </div>
                 <div className="carousel-item">
-                    <img src={brand6} className="rounded-box" />
+                    <a target="_blank" href="https://www.smlisuzu.com/">
+                        <img src={brand6} className="rounded-box" />
+                    </a>
                 </div>
             </div>
         </div>
